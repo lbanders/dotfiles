cb372973e48cb4ef88727f22757183daae919f4c
Leif Bredgaard Honore <leif@acosys.dk>
2024-04-16 19:38:56 +0200

    Updated layout etc

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -1,11 +1,52 @@
 return {
+  -- for DAP support
+  { "mfussenegger/nvim-dap" },
   {
     "akinsho/flutter-tools.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
-    },
-    config = true,
+    dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
+    config = function()
+      require('flutter-tools').setup {
+        debugger = {
+          -- make these two params true to enable debug mode
+          enabled = true,
+          run_via_dap = true,
+          register_configurations = function(_)
+
+             require("dap").adapters.dart = {
+                type = "executable",
+                command = vim.fn.stdpath("data") .. "/mason/bin/dart-debug-adapter",
+                args = {"flutter"}
+              }
+
+            require("dap").configurations.dart = {
+              {
+                type = "dart",
+                request = "launch",
+                name = "Launch flutter",
+                dartSdkPath = 'home/flutter/bin/cache/dart-sdk/',
+                flutterSdkPath = "home/flutter",
+                program = "${workspaceFolder}/lib/main.dart",
+                cwd = "${workspaceFolder}",
+              }
+            }
+            -- uncomment below line if you've launch.json file already in your vscode setup
+            -- require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          -- toggle it when you run without DAP
+          enabled = false,
+          open_cmd = "tabedit",
+        },
+        lsp = {
+          on_attach = require("vim.lsp").common_on_attach,
+          capabilities = require("vim.lsp").default_capabilities,
+        },
+      }
+    end
+  },
+  -- for dart syntax hightling
+  {
+    "dart-lang/dart-vim-plugin"
   },
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "catppuccin-mocha",
+        theme = "nordic",
         sections = {
           lualine_a = {
             file = 1,
